c6a6caa33be54db2d2549ed5a6bcd9e35dbecfa4
noureldinelhelw <44075933+noureldinelhelw@users.noreply.github.com>
2022-03-27 13:50:52 -0400

    Added Top Comment

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-# This code is diffeerent from Mr. Carlisle's because his code had some errors and I fixed them. I also added some comments to make thing a little bit clearer.
+# This code is diffeerent from Mr. Carlisle's because his code had some errors and I fixed them. I also added some comments to make things a little bit clearer.
 
 #!/usr/bin/python3
 import string
