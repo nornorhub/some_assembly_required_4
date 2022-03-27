ca4ff83cc74f573b49f10f09ba7b5e3188c00c0b
noureldinelhelw <44075933+noureldinelhelw@users.noreply.github.com>
2022-03-27 12:28:19 -0400

    Add solution.py

---
+++ solution.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import string
+import itertools
+
+from wasmtime import Store, Module, Instance, Trap, MemoryType, Memory, Limits, WasmtimeError
+
+store = Store()
+module = Module.from_file(store.engine, "assembly4.wat")
+instance = Instance(store, module, [])
+
+exports = instance.exports(store)
+memory = exports["memory"]
+check_flag = exports["check_flag"]
+
+def init_flag(pair):
+   for i in range(len(flag)):
+      memory.data_ptr(store)[1072+i]=ord(flag[i])
+   for j in range(len(flag_so_far)):
+      memory.data_ptr(store)[1072+8+j]=ord(flag_so_far[j])
+   memory.data_ptr(store)[1072+8+len(flag_so_far)]=ord(pair[0])
+   memory.data_ptr(store)[1072+8+len(flag_so_far)+1]=ord(pair[1])
+
+def print_test_flag():
+   g=''
+   for j in range(1072,1072+8+2+len(flag_so_far)):
+      g=g+chr(memory.data_ptr(store)[j])
+   print(g)
+
+def count_match():
+   count = 0
+   while(memory.data_ptr(store)[1024+count]==memory.data_ptr(store)[1072+count]):
+      count=count+1
+   return count
+
+flag="picoCTF{0123456789abcdef0123456789abcdef}"
+flag_chars=string.ascii_lowercase+"_"+string.digits+"}\x00"
+flag_so_far=""
+for i in range(24):
+   for j in itertools.product(flag_chars,repeat=2):
+      init_flag(j)
+      check_flag(store)
+      if(count_match()>=10+len(flag_so_far)):
+         flag_so_far+=(j[0]+j[1])
+         print("picoCTF{"+flag_so_far)
+         break
